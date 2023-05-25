383cec706bb202651aa54cd04d0660dbd212b2d1
DamianLarrea <damiantlarrea@gmail.com>
2023-05-26 00:49:39 +1000

    Add CORS support

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+const string allowedOriginsPolicy = "allowedOriginsPolicy";
+var allowedOrigins = builder.Configuration["AllowedOrigins"]?.Split(',') ?? Array.Empty<string>();
+
+builder.Services.AddCors(opts => {
+    opts.AddPolicy(allowedOriginsPolicy, policy => policy.WithOrigins(allowedOrigins));
+});
+
 builder.Services.AddControllers();
 builder.Services.AddCustomerControllerDependencies();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(allowedOriginsPolicy);
 
 app.UseHttpsRedirection();
 
