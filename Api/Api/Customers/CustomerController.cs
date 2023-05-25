70174a2be4b656bdeeaaa3afb5a4b0576b7b9567
DamianLarrea <damiantlarrea@gmail.com>
2023-05-25 22:12:12 +1000

    Create API, Application and Data Projects

---
+++ Api/Api/Customers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Customers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<CustomerDto> Get() => new List<CustomerDto>();
+
+        [HttpGet("{id}")]
+        public CustomerDto Get(Guid id) => new CustomerDto();
+
+        [HttpPost]
+        public void Post([FromBody] CustomerDto dto) { }
+    }
+}
---
+++ Api/Api/Customers/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Customers
+{
+    public class CustomerDto
+    {
+        public Guid? Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Mobile { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
